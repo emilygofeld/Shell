6804868229338d2b0d1bec4cdff82b69b20e1d0a
emilygofeld <emiloren17@gmail.com>
2024-12-03 19:38:49 +0200

    second stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        System.out.printf("%s command not found\n", input);
+        System.out.printf("%s: command not found\n", input);
     }
 }
