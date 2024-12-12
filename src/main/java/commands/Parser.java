d34f35f79137b93e97211f4d9519810b7d179bbe
emilygofeld <emiloren17@gmail.com>
2024-12-12 15:21:00 +0200

    added cd dir cmd

--- src/main/java/commands/Parser.java
+++ src/main/java/commands/Parser.java
@@ -28,7 +28,7 @@ public class Parser {
             case PWD ->
                     PathHandler.workingDir + "\n";
             case CD ->
-                cdCommand(data);
+                cdCommand(data) + "\n";
             case EXIT ->
                     "";
             case null ->
