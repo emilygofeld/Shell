b013fc656072f98e8dd3baa9db947559e8ab7368
emilygofeld <emiloren17@gmail.com>
2024-12-13 10:58:55 +0200

    edited cat command

--- src/main/java/commands/HandleCommands.java
+++ src/main/java/commands/HandleCommands.java
@@ -28,10 +28,9 @@ public class HandleCommands {
             for (String fileName : data.split(" ")) {
                 String trimmedFileName = fileName.replace("'", "");
                 File file = new File(trimmedFileName);
-
+                System.out.printf("\nfileName = %s, trimmedName = %s", fileName, trimmedFileName);
                 if (file.exists()) {
                     content.add(Files.readString(Path.of(workingDir + "/" + fileName)));
-                    System.out.println(content.getFirst());
                 }
 
             }
