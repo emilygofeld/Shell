c769433ac2414c27c7806406df8da7adc583cf2a
emilygofeld <emiloren17@gmail.com>
2024-12-13 11:36:18 +0200

    removed cat from builtin command

--- src/main/java/commands/HandleCommands.java
+++ src/main/java/commands/HandleCommands.java
@@ -52,7 +52,7 @@ public class HandleCommands {
             String[] fullCommand = new String[] {command, data};
             Process cmdProcess = Runtime.getRuntime().exec(fullCommand);
 
-            return new String(cmdProcess.getInputStream().readAllBytes());
+            return new String(cmdProcess.getInputStream().readAllBytes()) + "\n";
         } catch (IOException e) {
             final String cmd = (Objects.equals(data, ""))? command : command + " " + data;
             return commandNotFound(cmd);
